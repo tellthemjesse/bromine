a167666fdcedfe462af859f14d0bc5f71cc3545c
jesse <tellthemjesse@proton.me>
2026-01-29 17:33:20 +0300

    feat: NoOpHasher

---
+++ src/lib/hash.rs
@@ -0,0 +1,40 @@
+use core::{
+    hash::{BuildHasher, Hasher},
+};
+
+
+/// [`BuildHasher`] for types that already contain a high-quality hash.
+#[derive(Clone, Default)]
+pub struct NoOpHash;
+
+impl BuildHasher for NoOpHash {
+    type Hasher = NoOpHasher;
+
+    fn build_hasher(&self) -> Self::Hasher {
+        NoOpHasher(0)
+    }
+}
+
+#[doc(hidden)]
+pub struct NoOpHasher(u64);
+
+// This is for types that already contain a high-quality hash and want to skip
+// re-hashing that hash.
+impl Hasher for NoOpHasher {
+    fn finish(&self) -> u64 {
+        self.0
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        // This should never be called by consumers. Prefer to call `write_u64` instead.
+        // Don't break applications (slower fallback, just check in test):
+        self.0 = bytes.iter().fold(self.0, |hash, b| {
+            hash.rotate_left(8).wrapping_add(*b as u64)
+        });
+    }
+
+    #[inline]
+    fn write_u64(&mut self, i: u64) {
+        self.0 = i;
+    }
+}
--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,1 +1,3 @@
 pub mod ecs;
+pub mod query;
+mod hash;
