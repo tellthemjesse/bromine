a1fe2f328399f7d1e6d7bb1f2dbeec7a537f5999
jesse <tellthemjesse@proton.me>
2026-03-27 21:50:14 +0300

    add(window): new methods required for Game trait

--- engine/src/window/game.rs
+++ engine/src/window/game.rs
@@ -1,17 +1,13 @@
-use winit::{
-    application::ApplicationHandler,
-    event_loop::ActiveEventLoop,
-    window::WindowId,
-};
+use crate::ecs::World;
+use winit::{application::ApplicationHandler, event_loop::ActiveEventLoop, window::WindowId};
 
 pub trait Game: ApplicationHandler {
     /// Creates an OpenGL-compatible window, ready for rendering
-    fn make_window(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-    ) -> ::anyhow::Result<WindowId>;
+    fn make_window(&mut self, event_loop: &ActiveEventLoop) -> ::anyhow::Result<WindowId>;
+    fn world(&self) -> &World;
+    fn world_mut(&mut self) -> &mut World;
     /// Loads game resources
-    fn prep_game_world(&mut self);
+    fn init_world(&mut self);
     /// Drops game resources
-    fn drop_game_world(&mut self);
+    fn deinit_world(&mut self);
 }
