563caa74fe50ad2376129eea12dc7130b996f829
jesse <tellthemjesse@proton.me>
2026-04-12 23:01:24 +0300

    ref: rewrite gl_headless display impl

--- gl-headless/src/lib.rs
+++ gl-headless/src/lib.rs
@@ -1,108 +1,165 @@
-use glutin::{
-    config::ConfigTemplateBuilder,
-    context::{ContextAttributesBuilder, PossiblyCurrentContext},
-    display::GetGlDisplay,
-    prelude::{GlDisplay, NotCurrentGlContext, PossiblyCurrentGlContext},
-    surface::{Surface, SurfaceAttributesBuilder, WindowSurface},
+#![allow(dead_code)]
+
+use glutin::config::ConfigTemplateBuilder;
+use glutin::context::{
+    ContextApi, ContextAttributesBuilder, NotCurrentContext, PossiblyCurrentContext, Version,
 };
+use glutin::display::{Display, GetGlDisplay};
+use glutin::prelude::*;
+use glutin::surface::{Surface, SurfaceAttributesBuilder, WindowSurface};
 use glutin_winit::DisplayBuilder;
-use std::{ffi::CString, num::NonZeroU32};
-use winit::{
-    application::ApplicationHandler,
-    event::WindowEvent,
-    event_loop::{ActiveEventLoop, EventLoop},
-    platform::windows::{EventLoopBuilderExtWindows, WindowExtWindows},
-    window::{WindowAttributes, WindowId},
-};
+use raw_window_handle::HasWindowHandle;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop, EventLoopProxy};
+use winit::window::{Window, WindowId};
 
-pub struct GlHeadless {
-    run_fn: fn(),
-}
+use std::ffi::CString;
+use std::num::NonZeroU32;
+use std::sync::{Once, RwLock, mpsc::Sender};
+use std::thread;
 
-impl GlHeadless {
-    /// OpenGL functions are loaded once an event loop is active(e.g., by using [`run_once`] method).
-    /// Until then, this structure just wraps a closure
-    pub const fn new(run_fn: fn()) -> Self {
-        Self { run_fn }
+// There is a Wayland version of this extension trait but the X11 version also works on Wayland
+#[cfg(windows)]
+use winit::platform::windows::EventLoopBuilderExtWindows;
+#[cfg(unix)]
+use winit::platform::x11::EventLoopBuilderExtX11;
+
+type DisplayRequest = Sender<(Window, NotCurrentContext, Surface<WindowSurface>, Display)>;
+
+struct Tests {}
+
+impl ApplicationHandler<DisplayRequest> for Tests {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        _event: WindowEvent,
+    ) {
     }
 
-    /// Creates and runs an event loop that exits after closure execution.
-    /// That means no buffers are swapped
-    pub fn run_once(&mut self) -> anyhow::Result<()> {
-        let event_loop = EventLoop::builder().with_any_thread(true).build()?;
-        event_loop.run_app(self)?;
+    fn user_event(&mut self, event_loop: &ActiveEventLoop, request: DisplayRequest) {
+        let window_attributes = Window::default_attributes().with_visible(false);
+        let config_template_builder = ConfigTemplateBuilder::new();
+        let (window, gl_config) = DisplayBuilder::new()
+            .with_window_attributes(Some(window_attributes))
+            .build(event_loop, config_template_builder, |mut configs| {
+                configs.next().unwrap()
+            })
+            .unwrap();
+        let window = window.unwrap();
+        let raw_window_handle = window.window_handle().unwrap().as_raw();
+
+        let api = if cfg!(windows) {
+            ContextApi::OpenGl(Some(Version::new(4, 6)))
+        } else {
+            ContextApi::Gles(Some(Version::new(4, 6)))
+        };
+
+        let context_attributes = ContextAttributesBuilder::new()
+            .with_context_api(api)
+            .build(Some(raw_window_handle));
+
+        let dispay = gl_config.display();
         
-        Ok(())
+        let not_current_gl_context = unsafe {
+            dispay
+                .create_context(&gl_config, &context_attributes)
+                .unwrap()
+        };
+
+        let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+            raw_window_handle,
+            NonZeroU32::new(800).unwrap(),
+            NonZeroU32::new(600).unwrap(),
+        );
+
+        let surface = unsafe {
+            dispay
+                .create_window_surface(&gl_config, &attrs)
+                .unwrap()
+        };
+       
+        request
+            .send((window, not_current_gl_context, surface, dispay))
+            .unwrap();
     }
 }
 
-impl ApplicationHandler for GlHeadless {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let context = GlContext::new(&event_loop).unwrap();
-
-        if !context.is_current() { context.make_current(); }
-
-        (self.run_fn)();
-        event_loop.exit();
-    }
-    fn window_event(&mut self, _: &ActiveEventLoop, _: WindowId, _: WindowEvent) {}
+pub struct WindowDisplay {
+    pub context_surface: ContextSurfacePair,
+    pub window: Window,
+    pub display: Display,
 }
 
-struct GlContext {
+impl WindowDisplay {
+    pub fn load_gl(&self) {
+        gl::load_with(|addr| self.display.get_proc_address(&CString::new(addr).unwrap()));
+    }
+}
+
+pub struct ContextSurfacePair {
     context: PossiblyCurrentContext,
     surface: Surface<WindowSurface>,
 }
 
-impl GlContext {
-   fn new(event_loop: &ActiveEventLoop) -> anyhow::Result<Self> {
-        let window_attr = WindowAttributes::default()
-            .with_title("GlHeadless window")
-            .with_visible(false);
+impl ContextSurfacePair {
+    pub fn new(context: PossiblyCurrentContext, surface: Surface<WindowSurface>) -> Self {
+        Self { context, surface }
+    }
+}
 
-        let template_builder = ConfigTemplateBuilder::default();
-        let display_builder = DisplayBuilder::new().with_window_attributes(Some(window_attr));
+/// Builds a display 
+pub fn build_display() -> WindowDisplay {
+    static EVENT_LOOP_PROXY: RwLock<Option<EventLoopProxy<DisplayRequest>>> = RwLock::new(None);
+    static INIT_EVENT_LOOP: Once = Once::new();
 
-        let (window, config) = display_builder
-            .build(event_loop, template_builder, |mut configs| {
-                configs.next().unwrap()
+    // initialize event loop in a separate thread and store a proxy
+    INIT_EVENT_LOOP.call_once(|| {
+        let (sender, receiver) = std::sync::mpsc::channel();
+
+        thread::Builder::new()
+            .name("event_loop".into())
+            .spawn(move || {
+                let event_loop_res = if cfg!(unix) || cfg!(windows) {
+                    EventLoop::with_user_event().with_any_thread(true).build()
+                } else {
+                    EventLoop::with_user_event().build()
+                };
+                let event_loop = event_loop_res.expect("event loop building");
+
+                sender.send(event_loop.create_proxy()).unwrap();
+
+                let mut app = Tests {};
+                event_loop.run_app(&mut app).unwrap();
             })
-            .ok()
             .unwrap();
 
-        let window = window.unwrap();
-        let (w, h) = window.inner_size().into();
+        *EVENT_LOOP_PROXY.write().unwrap() = Some(receiver.recv().unwrap());
+    });
 
-        let window_handle = unsafe { window.window_handle_any_thread()? };
-        let rwh = window_handle.as_raw();
+    let (sender, receiver) = std::sync::mpsc::channel();
 
-        let surface_attr = unsafe {
-            SurfaceAttributesBuilder::<WindowSurface>::new().build(
-                rwh,
-                NonZeroU32::new_unchecked(w),
-                NonZeroU32::new_unchecked(h),
-            )
-        };
+    // request event loop create display building pieces and send them back
+    EVENT_LOOP_PROXY
+        .read()
+        .unwrap()
+        .as_ref()
+        .unwrap()
+        .send_event(sender)
+        .unwrap();
 
-        let context_attr = ContextAttributesBuilder::new().build(Some(rwh));
+    // block until required display building pieces are received
+    let (window, not_current_gl_context, surface, display) = receiver.recv().unwrap();
 
-        let display = config.display();
+    // now use our surface to make our context current and finally create our display
+    let current_context = not_current_gl_context.make_current(&surface).unwrap();
 
-        let not_current_ctx = unsafe { display.create_context(&config, &context_attr)? };
-
-        let surface = unsafe { display.create_window_surface(&config, &surface_attr)? };
-
-        let context = not_current_ctx.make_current(&surface)?;
-
-        gl::load_with(|addr| display.get_proc_address(&CString::new(addr).unwrap()));
-
-        Ok(GlContext { context, surface })
-    }
-
-    fn is_current(&self) -> bool {
-        self.context.is_current()
-    }
-
-    fn make_current(&self) {
-       self.context.make_current(&self.surface).unwrap()
+    WindowDisplay {
+        context_surface: ContextSurfacePair::new(current_context, surface),
+        window,
+        display,
     }
 }
